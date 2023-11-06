3774ea132670304ac3f9a83cd9b55598b0338f27
jihee <esder1310@naver.com>
2023-11-06 14:33:47 +0900

    :art: design : login page px->rem으로 수정

--- pages/login/Header.js
+++ pages/login/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { GlobalStyle } from '@/styles/GlobalStyle';
 
 const Header = () => {
     return(
         <>
+            <GlobalStyle />
             <LoginHeader>
                 <AiOutlineArrowLeft className={'AllowIcon'}/>
                 <p className={'HeaderLogin'}>로그인</p>
@@ -15,23 +17,26 @@ const Header = () => {
 
 const LoginHeader=styled.div`
   display: flex;
+  flex-direction: row;
+  text-align: center;
   align-items: center;
   //margin: 50px 527px 1526px 48px;
-  margin: 0 auto 0 0;
-
-  height: 48px;
+  margin: 3.13rem auto 0 3rem;
 
 
   .AllowIcon{
-    width: 48px;
-    height: 48px;
+    width: 3rem;
+    height: 3rem;
+    flex-shrink: 0;
+    
+    margin-right: 1.75rem;
   }
   .HeaderLogin{
-    margin: 0 0 0 28px;
-    font-size: 38px;
+    color: #000;
+    font-size: 2.375rem;
     font-style: normal;
     font-weight: 400;
-    line-height: 42px;
+    line-height: 2.625rem; /* 110.526% */
   }
 `;
 export default Header;
